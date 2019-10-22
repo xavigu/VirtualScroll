663b0578ea734dfe44b95f72fe9b2fdfaa1de027
xavigu <xavigu91@gmail.com>
2019-10-22 13:54:24 +0200

    Add buttons to go to the start, middle and end of the item list in the cdkviewport element

--- src/app/virtual/virtual.component.ts
+++ src/app/virtual/virtual.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { CdkVirtualScrollViewport } from '@angular/cdk/scrolling';
 
 @Component({
   selector: 'app-virtual',
@@ -7,11 +8,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class VirtualComponent implements OnInit {
 
+  @ViewChild(CdkVirtualScrollViewport) viewport: CdkVirtualScrollViewport; //referencia al elemento cdk-virtual-scroll-viewport del html
+
   personas = Array(500).fill(0);
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  goEnd(){
+    this.viewport.scrollToIndex(this.personas.length);
+  };
+
+  goStart(){
+    this.viewport.scrollToIndex(0);
+  };
+
+  goMiddle(){
+    this.viewport.scrollToIndex(this.personas.length/2);
   }
 
 }
